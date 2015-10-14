d577b96a0e88de9cbf7be249a222fe5740b73fbc
ozanpekmezci <ozanpekmezci@gmail.com>
2015-10-14 14:14:39 +0200

    First commit

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+  acts_as_authentic
+  def deliver_password_reset_instructions!
+  reset_perishable_token!
+  PasswordResetMailer.reset_email(self).deliver_now
+end
+end
